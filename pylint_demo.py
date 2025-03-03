c393700ce313be9c1d1e068e59d49ca762035c6d
frzn11 <fa2235@nyu.edu>
2025-03-03 15:19:39 +0400

    updated

--- pylint_demo.py
+++ pylint_demo.py
@@ -1,10 +1,23 @@
-def sum(a, b):
+"""
+This file has two functions.
+
+"""
+
+def sum_func(a, b):
+    """
+    This function returns the sum of two numbers.
+
+    """
     return a + b
 
 
 def hello(name):
+    """
+    This function prints Hello.
+
+    """
     print(f"Hello, {name}!")
 
 
 hello("Alice")
-hello()
+hello(None)
